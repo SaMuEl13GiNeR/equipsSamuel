30b43dba224cde6d85e0ae613483f389b3970495
SamuelGiner13 <samginurb@alu.edu.gva.es>
2022-11-30 17:34:41 +0100

    Versió 3.0 EquipsSamuel Serveis

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -1,16 +1,17 @@
 <?php
 namespace App\Controller;
+use App\Service\ServeiDadesEquips;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class EquipsController extends AbstractController
 {
-    private $equips = array(
-        array("codi" => "1", "nom" => "Equip Roig", "cicle" =>"DAW", "curs" =>"22/23", "membres" => array("Elena","Vicent","Joan","Maria")),
-        array("codi" => "2", "nom" => "Equip Verd", "cicle" =>"DAM", "curs" =>"21/22", "membres" => array("Pepe","Pepa","Pepito","Pepita")),
-        array("codi" => "3", "nom" => "Equip Blau", "cicle" =>"ASIX", "curs" =>"20/21", "membres" => array("Antonita","Antonito","Antonia","Antonio")),
-        array("codi" => "4", "nom" => "Equip Groc", "cicle" =>"SMX", "curs" =>"19/20", "membres" => array("Paquito","Paquita","Paco","Paca"))
-    );
+
+    private $equips;
+    public function __construct(ServeiDadesEquips $dades){
+        $this->equips = $dades->get();
+    }
+
 
     #[Route('/equip/{codi}', name:'dades_equips')]
     public function equip($codi = 1)
