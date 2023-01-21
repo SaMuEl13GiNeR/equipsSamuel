c548e45115919dd12e94ebd3deac66966eb462c5
SaMuEl13GiNeR <sam.gi2002@gmail.com>
2023-01-21 13:20:34 +0100

    Formulari Login Text Pla Samuel

---
+++ migrations/Version20230121112726.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230121112726 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE usuari (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_68CC94FFF85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP INDEX nom ON equip');
+        $this->addSql('DROP INDEX email ON membre');
+        $this->addSql('ALTER TABLE membre CHANGE nota nota NUMERIC(2, 2) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE usuari');
+        $this->addSql('CREATE UNIQUE INDEX nom ON equip (nom)');
+        $this->addSql('ALTER TABLE membre CHANGE nota nota NUMERIC(2, 0) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX email ON membre (email)');
+    }
+}
